255f871b3f58fb7368e3825987be43bf997ba87f
Dell <klan_1umut@hotmail.com>
2021-11-29 00:11:04 +0300

    Add project files.

---
+++ Deneme/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Deneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.AIaction = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(12, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(93, 56);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(111, 12);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(93, 56);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(210, 12);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(93, 56);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(12, 74);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(93, 56);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(111, 74);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(93, 56);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(210, 74);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(93, 56);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(12, 136);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(93, 56);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(111, 136);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(93, 56);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(210, 136);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(93, 56);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.HumanPlayer);
+            // 
+            // AIaction
+            // 
+            this.AIaction.Interval = 500;
+            this.AIaction.Tick += new System.EventHandler(this.AI);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 225);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Timer AIaction;
+    }
+}
+
---
+++ Deneme/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deneme
+{
+    public partial class Form1 : Form
+    {
+        Player Gameturn;
+        List<Button> buttons;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();         
+            Buttondefine();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public enum Player
+        {
+            X, O
+        }
+
+        void Buttondefine()
+        {
+            buttons = new List<Button> { b1, b2, b3, b4, b5, b6, b7, b8, b9 };
+        }
+ 
+        private void HumanPlayer(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            Gameturn = Player.X;
+            button.Text = Gameturn.ToString();
+            button.Enabled = false;
+            buttons.Remove(button);
+            GameControl();
+            AIaction.Start();
+        }
+
+        private void AI(object sender, EventArgs e)
+        {
+            if (buttons.Count > 0)
+            {
+                int index = rnd.Next(buttons.Count);
+                buttons[index].Enabled = false;
+                Gameturn = Player.O;
+                buttons[index].Text = Gameturn.ToString();
+                buttons.RemoveAt(index);
+                GameControl();
+                AIaction.Stop();
+            }
+        }
+
+        void GameControl()
+        {
+
+            if (b1.Text == "X" && b2.Text == "X" && b3.Text == "X"
+               || b4.Text == "X" && b5.Text == "X" && b6.Text == "X"
+               || b7.Text == "X" && b9.Text == "X" && b8.Text == "X"
+               || b1.Text == "X" && b4.Text == "X" && b7.Text == "X"
+               || b2.Text == "X" && b5.Text == "X" && b8.Text == "X"
+               || b3.Text == "X" && b6.Text == "X" && b9.Text == "X"
+               || b1.Text == "X" && b5.Text == "X" && b9.Text == "X"
+               || b3.Text == "X" && b5.Text == "X" && b7.Text == "X")
+            {
+                AIaction.Stop();
+                MessageBox.Show("KAZANDINIZ.");
+            }
+
+            else if (b1.Text == "O" && b2.Text == "O" && b3.Text == "O"
+            || b4.Text == "O" && b5.Text == "O" && b6.Text == "O"
+            || b7.Text == "O" && b9.Text == "O" && b8.Text == "O"
+            || b1.Text == "O" && b4.Text == "O" && b7.Text == "O"
+            || b2.Text == "O" && b5.Text == "O" && b8.Text == "O"
+            || b3.Text == "O" && b6.Text == "O" && b9.Text == "O"
+            || b1.Text == "O" && b5.Text == "O" && b9.Text == "O"
+            || b3.Text == "O" && b5.Text == "O" && b7.Text == "O")
+            {
+                AIaction.Stop();
+                MessageBox.Show("KAYBETTİNİZ."); 
+            }
+        }
+    }
+}
